759c09301046b81fc76cb9d44aa95d46eca40db9
Daniil Protasenia <danikprotaseney@gmail.com>
2024-01-16 00:24:31 +0300

    feat(locustfile): add type hint

--- locustfile.py
+++ locustfile.py
@@ -5,29 +5,29 @@ class MyUser(HttpUser):
     wait_time = between(1, 3)
 
     @task
-    def hello(self):
+    def hello(self) -> None:
         self.client.get("/hello")
 
     @task
-    def io_task(self):
+    def io_task(self) -> None:
         self.client.get("/io_task")
 
     @task
-    def cpu_task(self):
+    def cpu_task(self) -> None:
         self.client.get("/cpu_task")
 
     @task
-    def random_sleep(self):
+    def random_sleep(self) -> None:
         self.client.get("/random_sleep")
 
     @task
-    def random_status(self):
+    def random_status(self) -> None:
         self.client.get("/random_status")
 
     @task
-    def chain(self):
+    def chain(self) -> None:
         self.client.get("/chain")
 
     @task
-    def error_test(self):
+    def error_test(self) -> None:
         self.client.get("/error_test")

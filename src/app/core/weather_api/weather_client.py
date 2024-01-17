29988243723fe8752a6aa34672bb7871b95fa94a
Daniil Protasenia <danikprotaseney@gmail.com>
2024-01-17 16:40:09 +0300

    feat(weather_client): add weather_client.py

---
+++ src/app/core/weather_api/weather_client.py
@@ -0,0 +1,39 @@
+import httpx
+from pydantic import BaseModel
+
+
+class CurrentWeatherData(BaseModel):
+    temperature: float
+    windspeed: float
+    winddirection: int
+    weathercode: int
+
+
+class WeatherClient:
+    BASE_URL = 'https://api.open-meteo.com/v1/'
+
+    async def fetch_current_weather_async(self, latitude: float, longitude: float) -> CurrentWeatherData:
+        url = f"{self.BASE_URL}forecast"
+        params = {
+            'latitude': latitude,
+            'longitude': longitude,
+            'current_weather': True
+        }
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, params=params)
+            response.raise_for_status()
+            data = response.json()['current_weather']
+            return CurrentWeatherData(**data)
+
+    def fetch_current_weather_sync(self, latitude: float, longitude: float) -> CurrentWeatherData:
+        url = f"{self.BASE_URL}forecast"
+        params = {
+            'latitude': latitude,
+            'longitude': longitude,
+            'current_weather': True
+        }
+        with httpx.Client() as client:
+            response = client.get(url, params=params)
+            response.raise_for_status()
+            data = response.json()['current_weather']
+            return CurrentWeatherData(**data)

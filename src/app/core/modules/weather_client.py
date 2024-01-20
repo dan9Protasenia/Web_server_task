cfa6f15ff2dafb4d32e07de96ea190e20556279a
Daniil Protasenia <danikprotaseney@gmail.com>
2024-01-20 15:04:43 +0300

    refactor(weather_client): moved basemodel and refactor code with isort, black and flake8

--- src/app/core/modules/weather_client.py
+++ src/app/core/modules/weather_client.py
@@ -1,39 +1,32 @@
 import httpx
-from pydantic import BaseModel
 
-
-class CurrentWeatherData(BaseModel):
-    temperature: float
-    windspeed: float
-    winddirection: int
-    weathercode: int
+from ..schemas.data import CurrentWeatherData
 
 
 class WeatherClient:
-    BASE_URL = 'https://api.open-meteo.com/v1/'
+    BASE_URL = "https://api.open-meteo.com/v1/"
 
     async def fetch_current_weather_async(self, latitude: float, longitude: float) -> CurrentWeatherData:
         url = f"{self.BASE_URL}forecast"
         params = {
-            'latitude': latitude,
-            'longitude': longitude,
-            'current_weather': True
-        }
+            "latitude": latitude,
+            "longitude": longitude,
+            "current_weather": True,
+                  }
         async with httpx.AsyncClient() as client:
             response = await client.get(url, params=params)
             response.raise_for_status()
-            data = response.json()['current_weather']
+            data = response.json()["current_weather"]
             return CurrentWeatherData(**data)
 
     def fetch_current_weather_sync(self, latitude: float, longitude: float) -> CurrentWeatherData:
         url = f"{self.BASE_URL}forecast"
         params = {
-            'latitude': latitude,
-            'longitude': longitude,
-            'current_weather': True
-        }
+            "latitude": latitude,
+            "longitude": longitude,
+            "current_weather": True,}
         with httpx.Client() as client:
             response = client.get(url, params=params)
             response.raise_for_status()
-            data = response.json()['current_weather']
+            data = response.json()["current_weather"]
             return CurrentWeatherData(**data)

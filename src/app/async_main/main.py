262920447e878f93066ce128dea6869b7687d2d8
Daniil Protasenia <danikprotaseney@gmail.com>
2024-01-20 15:06:20 +0300

    refactor(main): reformat imports for better readability

--- src/app/async_main/main.py
+++ src/app/async_main/main.py
@@ -3,13 +3,22 @@ import json
 import random
 
 from aiohttp import web
+from aiohttp.web import (
+    Application,
+    HTTPBadRequest,
+    HTTPException,
+    HTTPInternalServerError,
+    Request,
+    Response,
+    json_response,
+)
 
-from app.core.weather_api.weather_client import WeatherClient
-from app.async_main.consant import HTTPResponse, Path, SyncHost, SyncPort
+from ..core.enums.consant_async import HTTPResponse, Path, SyncHost, SyncPort
+from ..core.modules.weather_client import WeatherClient
 
 
-async def custom_header_middleware(app: web.Application, handler) -> web.Response:
-    async def middleware_handler(request: web.Request) -> web.Response:
+async def custom_header_middleware(app: Application, handler) -> Response:
+    async def middleware_handler(request: Request) -> Response:
         response = await handler(request)
         response.headers["Custom-Header"] = HTTPResponse.CUSTOM_HEADER.value
         return response
@@ -17,107 +26,109 @@ async def custom_header_middleware(app: web.Application, handler) -> web.Respons
     return middleware_handler
 
 
-async def hello(request: web.Request) -> web.Response:
-    return web.Response(text=HTTPResponse.HELLO.value)
+async def hello(request: Request) -> Response:
+    return Response(text=HTTPResponse.HELLO.value)
 
 
-async def echo(request: web.Request) -> web.Response:
+async def echo(request: Request) -> Response:
     try:
         data = await request.json()
     except json.JSONDecodeError:
-        raise web.HTTPBadRequest(text="Invalid JSON")
+        raise HTTPBadRequest(text="Invalid JSON")
 
-    return web.json_response(data)
+    return json_response(data)
 
 
-async def io_task(request: web.Request) -> web.Response:
+async def io_task(request: Request) -> Response:
     await asyncio.sleep(1)
 
-    return web.Response(text=HTTPResponse.IO_TASK.value)
+    return Response(text=HTTPResponse.IO_TASK.value)
 
 
-async def cpu_task(request: web.Request) -> web.Response:
+async def cpu_task(request: Request) -> Response:
     result = sum(i for i in range(1000000))
 
-    return web.Response(text=HTTPResponse.CPU_TASK.value.format(result=result))
+    return Response(text=HTTPResponse.CPU_TASK.value.format(result=result))
 
 
-async def random_sleep(request: web.Request) -> web.Response:
+async def random_sleep(request: Request) -> Response:
     sleep_time = random.uniform(0.1, 1.0)
     await asyncio.sleep(sleep_time)
 
-    return web.Response(text=HTTPResponse.RANDOM_SLEEP.value.format(sleep_time=sleep_time))
+    return Response(text=HTTPResponse.RANDOM_SLEEP.value.format(sleep_time=sleep_time))
 
 
-async def random_status(request: web.Request) -> web.Response:
+async def random_status(request: Request) -> Response:
     statuses = [200, 404, 500]
     status = random.choice(statuses)
 
-    return web.Response(status=status, text=HTTPResponse.RANDOM_STATUS.value.format(status=status))
+    return Response(status=status, text=HTTPResponse.RANDOM_STATUS.value.format(status=status))
 
 
-async def chain(request: web.Request) -> web.Response:
+async def chain(request: Request) -> Response:
     await asyncio.sleep(0.5)
     response_text = HTTPResponse.CHAIN_STEP_1.value
     await asyncio.sleep(0.5)
     response_text += HTTPResponse.CHAIN_STEP_2.value
 
-    return web.Response(text=response_text)
+    return Response(text=response_text)
 
 
-async def handle_404(request: web.Request) -> web.Response:
-    return web.Response(text=HTTPResponse.NOT_FOUND.value, status=404)
+async def handle_404(request: Request) -> Response:
+    return Response(text=HTTPResponse.NOT_FOUND.value, status=404)
 
 
-async def handle_500(request: web.Request) -> web.Response:
-    return web.Response(text=HTTPResponse.ERROR_500.value, status=500)
+async def handle_500(request: Request) -> Response:
+    return Response(text=HTTPResponse.ERROR_500.value, status=500)
 
 
-async def error_test(request: web.Request) -> web.Response:
-    raise web.HTTPInternalServerError(text=HTTPResponse.ERROR_TEST.value)
+async def error_test(request: Request) -> Response:
+    raise HTTPInternalServerError(text=HTTPResponse.ERROR_TEST.value)
 
 
-async def error_middleware(app: web.Application, handler) -> web.Response:
-    async def middleware_handler(request: web.Request) -> web.Response:
+async def error_middleware(app: Application, handler) -> Response:
+    async def middleware_handler(request: Request) -> Response:
         try:
             response = await handler(request)
-            return response
 
-        except web.HTTPException as ex:
+        except HTTPException as ex:
             if ex.status == 404:
-                return await handle_404(request)
-
+                response = await handle_404(request)
             elif ex.status == 500:
-                return await handle_500(request)
-            raise
+                response = await handle_500(request)
+            else:
+                raise
 
         except Exception as ex:
             print(f"Unexpected exception: {ex}")
-            return await handle_500(request)
+            response = await handle_500(request)
+
+        return response
 
     return middleware_handler
 
 
-async def weather(request: web.Request) -> web.Request:
-    latitude = request.query.get('latitude')
-    longitude = request.query.get('longitude')
+async def weather(request: Request) -> Request:
+    latitude = request.query.get("latitude")
+    longitude = request.query.get("longitude")
 
     if not latitude or not longitude:
-        raise web.HTTPBadRequest(reason="Missing latitude or longitude parameter")
+        raise HTTPBadRequest(reason="Missing latitude or longitude parameter")
 
     try:
         weather_client = WeatherClient()
-        current_weather = await weather_client.fetch_current_weather_async(latitude=float(latitude),
-                                                                     longitude=float(longitude))
-        return web.json_response(current_weather.dict())
+        current_weather = await weather_client.fetch_current_weather_async(
+            latitude=float(latitude), longitude=float(longitude)
+        )
+        return json_response(current_weather.dict())
     except Exception as e:
         print(f"Error fetching weather data: {e}")
 
-        raise web.HTTPInternalServerError()
+        raise HTTPInternalServerError()
 
 
-async def create_app() -> web.Application:
-    app = web.Application(middlewares=[error_middleware, custom_header_middleware])
+async def create_app() -> Application:
+    app = Application(middlewares=[error_middleware, custom_header_middleware])
     app.router.add_get(Path.HELLO.value, hello)
     app.router.add_post(Path.ECHO.value, echo)
     app.router.add_get(Path.IO_TASK.value, io_task)

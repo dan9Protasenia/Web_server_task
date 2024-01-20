fb3aace6ac604df3a2b05941bcb5376387d21fa0
Daniil Protasenia <danikprotaseney@gmail.com>
2024-01-20 20:59:04 +0300

    refactor(test): edit some path for patch

--- tests/test_main_sync.py
+++ tests/test_main_sync.py
@@ -19,7 +19,7 @@ def test_hello(mock_conn: MagicMock) -> None:
 
 
 def test_io_task(mock_conn: MagicMock) -> None:
-    with patch("Web_server_task.src.app.sync.main.time.sleep") as mock_sleep:
+    with patch("src.app.sync.main.time.sleep") as mock_sleep:
         mock_conn.recv.return_value = HTTPTestRequests.IO_TASK.value.encode("utf-8")
         handle_request(mock_conn)
         mock_sleep.assert_called_once_with(1)
@@ -35,8 +35,8 @@ def test_cpu_task(mock_conn: MagicMock) -> None:
 
 
 def test_random_sleep(mock_conn: MagicMock) -> None:
-    with patch("Web_server_task.src.app.sync.main.random.uniform", return_value=0.5) as mock_random:
-        with patch("Web_server_task.src.app.sync.main.time.sleep") as mock_sleep:
+    with patch("src.app.sync.main.random.uniform", return_value=0.5) as mock_random:
+        with patch("src.app.sync.main.time.sleep") as mock_sleep:
             mock_conn.recv.return_value = HTTPTestRequests.RANDOM_SLEEP.value.encode("utf-8")
             handle_request(mock_conn)
             assert mock_random.call_args == call(0.1, 1.0)
@@ -46,7 +46,7 @@ def test_random_sleep(mock_conn: MagicMock) -> None:
 
 
 def test_random_status(mock_conn: MagicMock) -> None:
-    with patch("Web_server_task.src.app.sync.main.random.choice", return_value=404) as mock_random:
+    with patch("src.app.sync.main.random.choice", return_value=404) as mock_random:
         mock_conn.recv.return_value = HTTPTestRequests.RANDOM_STATUS.value.encode("utf-8")
         handle_request(mock_conn)
         assert mock_random.call_args == call([200, 404, 500])
